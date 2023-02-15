5f4d4056268957f95686f59fd56d62131ee75a5f
Lê Đức Vinh <48788311+xataradi@users.noreply.github.com>
2023-02-15 21:22:45 +0700

    update 2
    update 2

--- web-chat-app/signup.php
+++ web-chat-app/signup.php
@@ -1,55 +1,101 @@
-<!doctype html>
+<?php 
+  session_start();
+
+  if (!isset($_SESSION['username'])) {
+?>
+
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Web Chat App -Sign Up</title>
-    <link link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Chat App - Sign Up</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	<link rel="stylesheet" 
+	      href="css/style.css">
+	<link rel="icon" href="img/logo.png">
 </head>
-
 <body class="d-flex
-            justify-content-center
-            align-items-center
-            vh-100">
-    <div class="w-400 p-5 shadow rounded">
-        <form method="post" action="app/http/signup.php">
-            <div class="d-flex
-                        justify-content-center
-                        align-items-center
-                        flex-column">
-                <img src="img/chat.png">
-                <h3 class="display-4 fs-1 text-center">
-                    Sign Up</h3>
-            </div>
+             justify-content-center
+             align-items-center
+             vh-100">
+	 <div class="w-400 p-5 shadow rounded">
+	 	<form method="post" 
+	 	      action="app/http/signup.php"
+	 	      enctype="multipart/form-data">
+	 		<div class="d-flex
+	 		            justify-content-center
+	 		            align-items-center
+	 		            flex-column">
 
-            <div class="mb-3">
-                <label class="form-label">
-                    Name</label>
-                <input type="text" class='form-control'>
-            </div>
+	 		<img src="img/logo.png" 
+	 		     class="w-25">
+	 		<h3 class="display-4 fs-1 
+	 		           text-center">
+	 			       Sign Up</h3>   
+	 		</div>
 
-            <div class="mb-3">
-                <label class="form-label"> User name</label>
-                <input type="text" class="form-control">
-            </div>
+	 		<?php if (isset($_GET['error'])) { ?>
+	 		<div class="alert alert-warning" role="alert">
+			  <?php echo htmlspecialchars($_GET['error']);?>
+			</div>
+			<?php } 
+              
+              if (isset($_GET['name'])) {
+              	$name = $_GET['name'];
+              }else $name = '';
 
-            <div class="mb-3">
-                <label class="form-label"> Password</label>
-                <input type="password" class="form-control">
-            </div>
+              if (isset($_GET['username'])) {
+              	$username = $_GET['username'];
+              }else $username = '';
+			?>
 
-            <div class="mb-3">
-                <label class="form-label"> Profile Picture</label>
-                <input type="file" class="form-control">
-            </div>
+	 	  <div class="mb-3">
+		    <label class="form-label">
+		           Name</label>
+		    <input type="text"
+		           name="name"
+		           value="<?=$name?>" 
+		           class="form-control">
+		  </div>
 
-            <button type="submit" class="btn btn-primary">Sign Up</button>
-            <a href="index.php">Login</a>
-        </form>
-    </div>
+		  <div class="mb-3">
+		    <label class="form-label">
+		           User name</label>
+		    <input type="text" 
+		           class="form-control"
+		           value="<?=$username?>" 
+		           name="username">
+		  </div>
+
+
+		  <div class="mb-3">
+		    <label class="form-label">
+		           Password</label>
+		    <input type="password" 
+		           class="form-control"
+		           name="password">
+		  </div>
+
+		  <div class="mb-3">
+		    <label class="form-label">
+		           Profile Picture</label>
+		    <input type="file" 
+		           class="form-control"
+		           name="pp">
+		  </div>
+		  
+		  <button type="submit" 
+		          class="btn btn-primary">
+		          Sign Up</button>
+		  <a href="index.php">Login</a>
+		</form>
+	 </div>
 </body>
-
 </html>
+<?php
+  }else{
+  	header("Location: home.php");
+   	exit;
+  }
+ ?>
